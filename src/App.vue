6ab8ecc5e237944cfb7497a73b06f94e98addc65
Abylay Kurakbayev <abylay.kurakbayev@gmail.com>
2022-12-23 20:29:28 +0600

    feat: done part 1s

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TotalBalance :total="totalbalance" />
+    <BudgetList :list="list"  @deleteItem="onDeleteItem" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BudgetList from '@/components/BudgetList';
+import TotalBalance from '@/components/TotalBalance';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BudgetList,
+    TotalBalance
+  },
+  data: () => ({
+    list: {
+      1: {
+        type: 'INCOME',
+        value: 100,
+        comment: 'Some comment',
+        id: 1,
+      },
+      2: {
+        type: 'OUTCOME',
+        value: -80,
+        comment: 'Some comment',
+        id: 2,
+      },
+    }
+  }),
+  computed: {
+    totalBalance() {
+      return Object.values(this.list).reduce(
+        (acc, item) => acc + item.value,
+        0
+      )
+    }
+  },
+  methods: {
+    onDeleteItem(id) {
+      this.$delete(this.list, id)
+    }
   }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/elements/index'
 
 Vue.config.productionTip = false
 
